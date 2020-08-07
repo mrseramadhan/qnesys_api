f563bab6df28ab8622aedfd911c6ef544f69d4e4
donbay28 <donbaysaputra28@gmail.com>
2020-08-07 10:50:02 +0700

    Ssid Hotel

---
+++ app/Ms_Ssid_Hotel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ms_Ssid_Hotel extends Model
+{
+    protected $table = 'ms_ssid_hotel';
+    protected $fillable = ['id_hotel','ssid','password'];
+    protected $primaryKey = 'id_ssid_hotel';
+}
